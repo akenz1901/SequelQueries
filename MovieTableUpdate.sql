e48e3727a171b1541d68308fe70eddb393d63eba
akenz1901 <michael_aka1@icloud.com>
2021-04-24 16:32:15 +0000

    Movie Table with queries

---
+++ MovieTableUpdate.sql
@@ -0,0 +1,28 @@
+Alter table movie
+Add
+MovieTime	integer		not null;
+
+Alter table movie
+drop movieTitle;
+
+Alter table movie
+add
+MovieTitle		CHAR(50)	NULL;
+
+INSERT INTO reviewer VALUES(
+9021, "Michael Akinsanya");
+
+INSERT INTO rating VALUES(
+924,	9006,	7.3,  null
+);
+INSERT INTO rating VALUES(
+909,	9008, null 	,227235,
+912,     9011,     8.4,      null,
+918, 9016,	null, 	580301
+);
+INSERT INTO rating VALUES(
+912,     9011,     8.4,      null
+);
+INSERT INTO rating VALUES(
+909,	9008, null 	,227235
+);
